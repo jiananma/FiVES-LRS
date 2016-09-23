872eabf6fe39bf72c0062ab109064e619c4d2fa6
Jianan Ma <Jianan Ma>
2016-09-23 11:21:29 +0200

    add pic and music

--- Plugins/LangRenShaPlugin/LangRenShaPluginInitializer.cs
+++ Plugins/LangRenShaPlugin/LangRenShaPluginInitializer.cs
@@ -9,6 +9,11 @@ namespace LangRenShaPlugin
 {
     class LangRenShaPluginInitializer : IPluginInitializer
     {
+
+        private Entity thisEntity;
+        private bool gameCreated = false;
+
+
         public string Name
         {
             get { return "LangRenSha"; }
@@ -26,12 +31,46 @@ namespace LangRenShaPlugin
 
         public void Initialize()
         {
-            throw new NotImplementedException();
+            while (!gameCreated)
+            {
+
+            }
+            DefineComponents();
+            AddEntity();
+        }
+
+        private void DefineComponents()
+        {
+            ComponentDefinition statusComponent = new ComponentDefinition("status");
+            statusComponent.AddAttribute<int>("day");
+            statusComponent.AddAttribute<string>("step");
+            statusComponent.AddAttribute<bool>("witch_medicine_used");
+            statusComponent.AddAttribute<bool>("witch_poison_used");
+            statusComponent.AddAttribute<int>("wolf_killed");
+            statusComponent.AddAttribute<int>("witch_poisoned");
+            ComponentRegistry.Instance.Register(statusComponent);
+        }
+
+        private void AddEntity()
+        {
+            Entity e = new Entity();
+            e["status"]["day"].Suggest(0);
+
+
+
+
+            this.thisEntity = e;
         }
 
         public void Shutdown()
         {
-            throw new NotImplementedException();
         }
+
+        public void InputGameConfig(int gamers, string config)
+        {
+            //TODO
+            this.gameCreated = true;
+        }
+
     }
 }
